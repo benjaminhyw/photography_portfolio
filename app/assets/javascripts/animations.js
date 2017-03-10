ee1f99ec8bbcfeebb1396800cb504fc9a35aab15
Ben Flores <benjaminhyw@gmail.com>
2017-03-10 11:16:42 -0800

    Add nav bar animation.  Still needs some tweaking

--- app/assets/javascripts/animations.js
+++ app/assets/javascripts/animations.js
@@ -1,7 +1,20 @@
+// Shorthand for Document Ready
 $(function(){
+
+  // This is what does the downward scroll animation
   $("#arrow-pic").on("click", function(event){
     $('html, body').animate({
     scrollTop: $("#photo-container").offset().top
     }, 1500, "swing");
-  })
+  });
+
+  $(window).on("scroll", function(){
+    if($(window).scrollTop() > 110) {
+      $("#nav").addClass("active");
+    } else {
+      //remove the background property so it comes transparent again (defined in your css)
+      $("#nav").removeClass("active");
+    }
+  });
+
 })

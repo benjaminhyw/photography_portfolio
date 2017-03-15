871f00a1ded4166908238a5e10de711521f8441d
Ben Flores <benjaminhyw@gmail.com>
2017-03-14 19:10:14 -0700

    Update migration, user registration form now takes name, instagram, facebook, and twitter as well as default fields.  Run migration

--- db/migrate/20170311005652_add_devise_to_users.rb
+++ db/migrate/20170311005652_add_devise_to_users.rb
@@ -2,8 +2,13 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.0]
   def self.up
     create_table :users do |t|
       ## Database authenticatable
+      t.string :name,               null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :instagram
+      t.string :facebook
+      t.string :twitter
+
 
       ## Recoverable
       t.string   :reset_password_token

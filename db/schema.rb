d573ff78511babcb155827f010b7ee9b0b8a1352
Ben Flores <benjaminhyw@gmail.com>
2017-03-17 12:34:09 -0700

    Run migration after generating photo model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170317191835) do
+ActiveRecord::Schema.define(version: 20170317193128) do
+
+  create_table "photos", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name",                   default: "",         null: false

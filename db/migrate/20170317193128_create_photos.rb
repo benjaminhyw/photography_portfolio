c55044f3eb428ec16343fc1db4ef72ceec458fe1
Ben Flores <benjaminhyw@gmail.com>
2017-03-17 12:53:49 -0700

    Update migration so photos now have titles and urls, run migration

--- db/migrate/20170317193128_create_photos.rb
+++ db/migrate/20170317193128_create_photos.rb
@@ -2,6 +2,8 @@ class CreatePhotos < ActiveRecord::Migration[5.0]
   def change
     create_table :photos do |t|
       t.integer :user_id
+      t.string :title
+      t.string :url
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,8 @@ ActiveRecord::Schema.define(version: 20170317193128) do
 
   create_table "photos", force: :cascade do |t|
     t.integer  "user_id"
+    t.string   "title"
+    t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

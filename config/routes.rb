fd2e9956fa1a57557ff5f2c5d7aeed93e223bf8e
Ben Flores <benjaminhyw@gmail.com>
2017-03-17 13:37:47 -0700

    Begin updating routes to give them custom pathnames

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'photos#index'
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
 end

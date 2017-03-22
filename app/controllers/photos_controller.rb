010652780d202ad3fb939ddf890e32ca2b5a482f
Ben Flores <benjaminhyw@gmail.com>
2017-03-21 18:47:26 -0700

    Create and sandbox PhotosController#next.  Notes are included as this is still in progress.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,6 +7,17 @@ class PhotosController < ApplicationController
     @pics = Photo.order(:id).limit(2).offset(@page * 10)
   end
 
+
+  # This method is supposed to fire when user clicks the next button/link.  It's supposed to simply display the next few images.. but currently only partially displays new images
+  def next
+    page ||= 1
+    page += 1
+
+    # I'm playing around with this right now... the 5 currently shows 5 pictures, which is fine but then clicking again won't show any new pics.. so maybe this isn't actually where the problem is.. I think it has to do with page?
+    @pics = Photo.find_pics_on_page(page, 5)
+    render :index
+  end
+
   private
     def set_page
       @page = params[:page] || 0

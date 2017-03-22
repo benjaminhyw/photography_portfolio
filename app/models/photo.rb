326a4193cadd8d00bf6005c46c303ce841b286d2
Ben Flores <benjaminhyw@gmail.com>
2017-03-21 18:48:08 -0700

    Create and sandbox class method Photo#self.find_pics_on_page.  Needs work.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,9 @@
 class Photo < ApplicationRecord
   belongs_to :user
+
+  def self.find_pics_on_page(page, amount)
+    Photo.where({
+      id: (((page * amount) - amount) ... (page * amount))
+    }) # you can figure this out, depending on if you want to rank by updated_at or other filter
+  end
 end

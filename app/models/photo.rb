bb56508bc020d87ffe495edd2caa36e1cc764252
Ben Flores <benjaminhyw@gmail.com>
2017-03-23 18:41:27 -0700

    Rename variable math to pic_range for specificity, remove comments that are no longer needed

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,8 +2,10 @@ class Photo < ApplicationRecord
   belongs_to :user
 
   def self.find_pics_on_page(page, amount)
+    pic_range = (((page * amount) - amount) + 1) .. (page * amount)
+
     Photo.where({
-      id: (((page * amount + 1) - amount) ... (page * amount + 1))
-    }) # you can figure this out, depending on if you want to rank by updated_at or other filter
+      id: pic_range
+    })
   end
 end

23e34209b9af862ffdd082ec7c5de7632c02573a
Ben Flores <benjaminhyw@gmail.com>
2017-03-23 18:39:49 -0700

    Clean up code, remove comments because issue is resolved

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,23 +7,14 @@ class PhotosController < ApplicationController
     @pics = Photo.order(:id).limit(3).offset(@page * 3)
   end
 
-
-  # This method is supposed to fire when user clicks the next button/link.  It's supposed to simply display the next few images.. but currently only partially displays new images
   def next
-
-    # The instance variables below were regular variables, no instance prior to removing "next" link and route.  You may have to play with this idea a little.
-    @page ||= 1
     @page += 1
-
-
-
-    # I'm playing around with this right now... the 5 currently shows 5 pictures, which is fine but then clicking again won't show any new pics.. so maybe this isn't actually where the problem is.. I think it has to do with page?
     @pics = Photo.find_pics_on_page(@page, 3)
     render :index
   end
 
   private
     def set_page
-      @page = params[:page] || 0
+      @page = params[:id].to_i
     end
 end

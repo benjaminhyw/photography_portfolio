fe14c647e8df74303ee4b47fc1ea58b90a2e8636
Ben Flores <benjaminhyw@gmail.com>
2017-03-23 18:42:53 -0700

    Clean up routes, remove dead comments and establish correct route for next page feature

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'photos#index'
 
-  # These next two routes exist because I am trying to figure out next page business.  These might change.
-  get '/', to: 'photos#index'
-  get 'photos/next', 'photos#next'
-
+  get 'photos/:id', to: 'photos#next', as: 'next'
 
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'

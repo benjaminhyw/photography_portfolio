f063139e4c1573566723d290dce100774457bb2e
Ben Flores <benjaminhyw@gmail.com>
2017-03-24 15:19:18 -0700

    Create instance variables in private PhotosController#set_page

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,19 +3,22 @@ class PhotosController < ApplicationController
   before_action :set_page, only: [:index, :next]
 
   def index
-    @photos = Photo.all
-    @pics = Photo.order(:id).limit(3).offset(@page * 3)
+    @pics = Photo.order(:id).limit(@pics_per_page).offset(@page * @pics_per_page)
+
   end
 
   def next
-    @page
-    @pics = Photo.find_pics_on_page(@page, 3)
+    @pics = Photo.find_pics_on_page(@page, @pics_per_page)
     render :index
   end
 
   private
     def set_page
+      @photos = Photo.all
       @page = params[:id].to_i
+      @pics_per_page = 3
+      @total_pic_qty = (@photos.count / @pics_per_page)
+      @page_max = @total_pic_qty + 1
 
       if @page != 0
         @link = @page + 1

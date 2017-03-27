352da6050d79dfb288ca6d3b0bce7593a96192e9
Ben Flores <benjaminhyw@gmail.com>
2017-03-27 14:54:43 -0700

    Comment out element_in_scroll to attempt different approach

--- app/assets/javascripts/infinite_pagination.js
+++ app/assets/javascripts/infinite_pagination.js
@@ -1,9 +1,9 @@
-function element_in_scroll(elem){
-  var docViewLeft = $(window).scrollLeft();
-  var docViewRight = docViewLeft + $(window).width();
+// function element_in_scroll(elem){
+//   var docViewLeft = $(window).scrollLeft();
+//   var docViewRight = docViewLeft + $(window).width();
 
-  var elemLeft = $(elem).offset().left;
-  var elemRight = elemLeft + $(elem).width();
+//   var elemLeft = $(elem).offset().left;
+//   var elemRight = elemLeft + $(elem).width();
 
-  return ((elemRight <= docViewRight) && (elemLeft >= docViewLeft));
-}
+//   return ((elemRight <= docViewRight) && (elemLeft >= docViewLeft));
+// }

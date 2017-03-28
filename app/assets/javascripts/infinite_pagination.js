f11e5989ef51f3c51feb7c066347ac9a005a9147
Ben Flores <benjaminhyw@gmail.com>
2017-03-27 23:32:41 -0700

    Apply AJAX for specified page, must make changes soon

--- app/assets/javascripts/infinite_pagination.js
+++ app/assets/javascripts/infinite_pagination.js
@@ -1,20 +1,20 @@
-// function element_in_scroll(elem){
-//   var docViewLeft = $(window).scrollLeft();
-//   var docViewRight = docViewLeft + $(window).width();
-
-//   var elemLeft = $(elem).offset().left;
-//   var elemRight = elemLeft + $(elem).width();
-
-//   return ((elemRight <= docViewRight) && (elemLeft >= docViewLeft));
-// }
-
-
 $(document).ready(function(){
   $("#next_button").on("click", function(event){
     event.preventDefault();
 
-    console.log("maybe")
+    // Gotta figure out how to make this number change accordingly.
+    var page = 2
 
-    $("#sidescrolling-container").append("new stuff")
+    var images = $.get("photos/" + page, function(data){
+      var info = $("#individual-photo", data).each(function(){
+        var title = $("#photo-title", this).text();
+
+        var url = $(this).data("url");
+
+        $("#sidescrolling-container").append(
+          '<div id="individual-photo"><img src="'+url+'" height="250px" width="250px"><span id="photo-title">'+title+'</span></div>'
+        )
+      })
+    })
   })
 });

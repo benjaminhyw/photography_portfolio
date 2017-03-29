ac1a8d00e7606fe6c56b4d8d638e0786b8130f6e
Ben Flores <benjaminhyw@gmail.com>
2017-03-29 12:41:17 -0700

    Add basic JavaScript for image modal

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,7 +1,26 @@
 $(document).ready(function(){
+  // Assign modal variable
+  var modal = document.getElementById('myModal');
+  var url = url || ""
+
+  // When the user clicks on picture, display modal
   $(".individual-photo").on("click", function(event){
-    event.preventDefault();
-    console.log("supppp");
+      url = $(this).data("url");
+      document.getElementById("modal-image").src=url;
+      modal.style.display = "block";
+      console.log(url)
   });
+
+  // When the user clicks on <span> (x), close the modal
+  $(".close").on("click", function(){
+    modal.style.display = "none";
+  })
+
+  // When the user clicks anywhere outside of the modal, close it
+  $(window).on("click", function(event){
+    if (event.target == modal){
+      modal.style.display = "none";
+    }
+  })
 
 })

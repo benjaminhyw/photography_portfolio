1ac642369dc8e316acf5c518a13fb2e64cdd34ab
Ben Flores <benjaminhyw@gmail.com>
2017-03-29 13:29:46 -0700

    Begin making JS changes that better reflect flex-box changes

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,25 +1,25 @@
 $(document).ready(function(){
   // Assign modal variable
-  var modal = document.getElementById('myModal');
+  var modal = document.getElementById('myModal-flex');
   var url = url || ""
 
   // When the user clicks on picture, display modal
   $(".individual-photo").on("click", function(event){
       url = $(this).data("url");
       document.getElementById("modal-image").src=url;
-      modal.style.display = "block";
+      modal.style.visibility = "visible";
       console.log(url)
   });
 
   // When the user clicks on <span> (x), close the modal
-  $(".close").on("click", function(){
-    modal.style.display = "none";
+  $(".close-flex").on("click", function(){
+    modal.style.visibility = "collapse";
   })
 
   // When the user clicks anywhere outside of the modal, close it
   $(window).on("click", function(event){
     if (event.target == modal){
-      modal.style.display = "none";
+      modal.style.visibility = "collapse";
     }
   })
 

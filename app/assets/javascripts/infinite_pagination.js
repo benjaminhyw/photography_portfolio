9ba1fa31181f99d9f354b0958ef5d8ae1e6a0b36
Ben Flores <benjaminhyw@gmail.com>
2017-03-30 14:07:00 -0700

    Create more variables out of new data sent through HTML

--- app/assets/javascripts/infinite_pagination.js
+++ app/assets/javascripts/infinite_pagination.js
@@ -10,9 +10,11 @@ $(document).ready(function(){
           var title = $("#photo-title", this).text();
 
           var url = $(this).data("url");
+          var photo_position = $(this).data("position");
+          var page_max = $(this).data("page-max");
 
           $("#sidescrolling-container").append(
-            '<div class="individual-photo"><img src="'+url+'" height="250px" width="250px"><span id="photo-title">'+title+'</span></div>'
+            '<div class="individual-photo" data-url="'+ url +'" data-position="<%= '+ photo_position +' %>"  data-page-max="<%= '+ page_max +' %>"><img src="'+url+'" height="250px" width="250px"><span id="photo-title">'+title+'</span></div>'
           )
         });
       });

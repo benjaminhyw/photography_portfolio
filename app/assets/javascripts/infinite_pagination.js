8472adb9ef708b5c0c4cc7c7b012355a8d5095b5
Ben Flores <benjaminhyw@gmail.com>
2017-03-30 12:59:36 -0700

    Move sidescroll logic inside a conditional that only works up to the page max

--- app/assets/javascripts/infinite_pagination.js
+++ app/assets/javascripts/infinite_pagination.js
@@ -1,20 +1,21 @@
 $(document).ready(function(){
   var count = 1;
+  var pageMax = $(".individual-photo").data("page-max");
 
   $("#sidescrolling-container").on("scroll", function(event){
+    if (count <= pageMax){
+      count++
+      var images = $.get("photos/" + count, function(data){
+        var info = $(".individual-photo", data).each(function(){
+          var title = $("#photo-title", this).text();
 
-    count++;
+          var url = $(this).data("url");
 
-    var images = $.get("photos/" + count, function(data){
-      var info = $(".individual-photo", data).each(function(){
-        var title = $("#photo-title", this).text();
-
-        var url = $(this).data("url");
-
-        $("#sidescrolling-container").append(
-          '<div class="individual-photo"><img src="'+url+'" height="250px" width="250px"><span id="photo-title">'+title+'</span></div>'
-        )
-      })
-    })
+          $("#sidescrolling-container").append(
+            '<div class="individual-photo"><img src="'+url+'" height="250px" width="250px"><span id="photo-title">'+title+'</span></div>'
+          )
+        });
+      });
+    };
   });
 });

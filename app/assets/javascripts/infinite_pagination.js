f231088596e95279911ad2542ef2cdcfcde19964
Ben Flores <benjaminhyw@gmail.com>
2017-03-31 17:05:33 -0700

    Leaving notes for future refrence

--- app/assets/javascripts/infinite_pagination.js
+++ app/assets/javascripts/infinite_pagination.js
@@ -2,6 +2,11 @@ $(document).ready(function(){
   var count = 1;
   var pageMax = $(".individual-photo").data("page-max");
 
+
+  // You need a new class on your AJAX Images
+  // Hide AJAX images
+  // On scroll, ONLY ON AJAXED IMAGES fade in
+
   $("#sidescrolling-container").on("scroll", function(event){
     if (count <= pageMax){
       count++
@@ -11,6 +16,8 @@ $(document).ready(function(){
           var title = $("#photo-title", this).text();
           var url = $(this).data("url");
           var photo_position = $(this).data("position");
+
+          // Find new place for page_max, kind of irrelevant here.
           var page_max = $(this).data("page-max");
           var decide_class = ""
 
@@ -21,7 +28,8 @@ $(document).ready(function(){
           }
 
           $("#sidescrolling-container").append(
-            '<div class="individual-photo ' + decide_class + '" data-url="'+ url +'" data-position="'+ photo_position +'"  data-page-max="<%= '+ page_max +' %>"><img class="actual-image" src="'+url+'"><span id="photo-title">'+title+'</span></div>'
+            // Making note, the class "ajax-image" currently has no function.  It started as an attempt to get horizontal load going, but still playing with the idea.
+            '<div class="individual-photo ajax-image' + decide_class + '" data-url="'+ url +'" data-position="'+ photo_position +'"  data-page-max="<%= '+ page_max +' %>"><img class="actual-image" src="'+url+'"><span id="photo-title">'+title+'</span></div>'
           );
 
         });

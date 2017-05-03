b06e77900e4a2ed6f12609fddd044b25c3b21465
Ben Flores <benjaminhyw@gmail.com>
2017-05-02 19:39:30 -0700

    Write initial JavaScript code that hides #footer-span text on click

--- app/assets/javascripts/contact.js
+++ app/assets/javascripts/contact.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
+  var footerSpan = document.getElementById('footer-span');
 
+  $(footerSpan).on('click', function(event){
+    event.preventDefault();
+    footerSpan.innerHTML = "";
+  })
 })

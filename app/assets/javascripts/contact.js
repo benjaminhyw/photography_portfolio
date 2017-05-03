79f10e9b24d569440516e1b1560b890a21a37222
Ben Flores <benjaminhyw@gmail.com>
2017-05-03 13:17:56 -0700

    Give footer a class when event is fired

--- app/assets/javascripts/contact.js
+++ app/assets/javascripts/contact.js
@@ -8,20 +8,17 @@ $(document).ready(function(){
   $(footerSpan).on('click', function(event){
     event.preventDefault();
     if ((text == "CONTACT") && (count < 1)){
-
       $(this).fadeOut(400);
-
       setTimeout(function(){
-
       // Here, I'm just adding all of the img tags for all of the social media links
       footerSpan.innerHTML =
         "<img class='social_media_icon' src='../assets/social_media_icons/white_fb_transparent_circle.png'><img class='social_media_icon' src='../assets/social_media_icons/white_twitter_transparent_circle.png'><img class='social_media_icon' src='../assets/social_media_icons/white_ig_transparent_circle.png'><img class='social_media_icon' src='../assets/social_media_icons/white_mail_transparent_circle.png'>";
       }, 500);
-
-      $(this).fadeIn(400);
-      count ++
-
+      $(footer).addClass('footer-color-change');
+      $(this).fadeIn(600);
+      count ++;
     };
+
   });
 
 });

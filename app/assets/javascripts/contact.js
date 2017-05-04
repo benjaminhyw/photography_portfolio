1810bdfab2ce83ee35d7377c95b58a179d7f0bcb
Ben Flores <benjaminhyw@gmail.com>
2017-05-03 20:26:59 -0700

    Minor edit to social media url

--- app/assets/javascripts/contact.js
+++ app/assets/javascripts/contact.js
@@ -13,9 +13,9 @@ $(document).ready(function(){
       // Here, I'm just adding all of the img tags for all of the social media links
       footerSpan.innerHTML =
         "<a class='social_media_link' href='#'><img class='social_media_icon' src='../assets/social_media_icons/white_fb_transparent_circle.png'></a>"+
-        "<a class='social_media_link' href='google.com'><img class='social_media_icon' src='../assets/social_media_icons/white_twitter_transparent_circle.png'></a>"+
+        "<a class='social_media_link' href='https://www.google.com'><img class='social_media_icon' src='../assets/social_media_icons/white_twitter_transparent_circle.png'></a>"+
         "<a class='social_media_link' href='#'><img class='social_media_icon' src='../assets/social_media_icons/white_ig_transparent_circle.png'></a>"+
-        "<a class='social_media_link' href='google.com'><img class='social_media_icon' src='../assets/social_media_icons/white_mail_transparent_circle.png'></a>";
+        "<a class='social_media_link' href='https://www.google.com'><img class='social_media_icon' src='../assets/social_media_icons/white_mail_transparent_circle.png'></a>";
       }, 500);
       $(footer).addClass('footer-color-change');
       $(this).fadeIn(600);

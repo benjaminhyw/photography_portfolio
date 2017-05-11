9c6d2211936033a338a2b5bfdbd54e8c569463c2
Ben Flores <benjaminhyw@gmail.com>
2017-05-11 12:46:18 -0700

    Remove setTimeout and adjust times for contact fades

--- app/assets/javascripts/contact.js
+++ app/assets/javascripts/contact.js
@@ -8,21 +8,16 @@ $(document).ready(function(){
 
   $(footerSpan).on('click', function(event){
     if ((text == "CONTACT") && (count < 1)){
-      $(footerUnderline).fadeOut(300);
-      event.preventDefault();
-      $(this).fadeOut(400);
-      setTimeout(function(){
-      // Here, I'm just adding all of the img tags for all of the social media links
-      // All of the links before need to be updated with the correct links, but they have placeholders for right now
+      $(footerUnderline).fadeOut(0);
+      $(this).fadeOut(0);
+      $(footerSpan).removeClass('footer-span-direction');
+      $(footer).addClass('footer-color-change');
       footerSpan.innerHTML =
         "<a class='social_media_link' href='#' target='_blank'><img id='facebook' class='social_media_icon' src='../assets/social_media_icons/white_fb_transparent_circle.png'></a>"+
         "<a class='social_media_link' href='https://www.google.com' target='_blank'><img id='twitter' class='social_media_icon' src='../assets/social_media_icons/white_twitter_transparent_circle.png'></a>"+
         "<a class='social_media_link' href='#' target='_blank'><img id='instagram' class='social_media_icon' src='../assets/social_media_icons/white_ig_transparent_circle.png'></a>"+
         "<a class='social_media_link' href='https://www.google.com' target='_blank'><img id='mail' class='social_media_icon' src='../assets/social_media_icons/white_mail_transparent_circle.png'></a>";
-        $(footerSpan).removeClass('footer-span-direction');
-      }, 500);
-      $(footer).addClass('footer-color-change');
-      $(this).fadeIn(600);
+      $(footerSpan).fadeIn(1500);
       count ++;
     };
 
